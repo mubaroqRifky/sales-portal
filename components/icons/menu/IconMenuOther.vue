c14e9c1c8472870bf1b2159af92e3ab455efddbc
mubaroqRifky <mubaroq.rifki92@gmail.com>
2026-04-27 16:54:04 +0700

    update

--- components/icons/menu/IconMenuOther.vue
+++ components/icons/menu/IconMenuOther.vue
@@ -3,8 +3,8 @@
         <svg
             :style="{ width, height }"
             xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
+            width="50"
+            height="50"
             viewBox="0 0 24 24"
         >
             <g fill="none">
